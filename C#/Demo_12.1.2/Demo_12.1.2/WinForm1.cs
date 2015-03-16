bc017a8af54cadd054437d6d88b171b84dfe7add
Bowen Li <517066422@qq.com>
2015-03-16 15:58:02 +0800

    Update 2015-03-16 1557
    Demo_12.1.2

---
+++ C#/Demo_12.1.2/Demo_12.1.2/WinForm1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo_12._1._2
+{
+    public partial class WinForm1 : Form
+    {
+        public WinForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WinForm2 winform2 = new WinForm2();
+            winform2.Show();
+        }
+
+    }
+}
