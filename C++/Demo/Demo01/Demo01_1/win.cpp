59113ebcaa019af9f18122fff1152473ee9acfcc
Bowen Li <517066422@qq.com>
2015-10-17 17:57:11 +0800

    Revert "Updat 2015-10-16"
    This reverts commit 3ca48c687a8292106135382b49661f7944292cb8.

--- C++/Demo/Demo01/Demo01_1/win.cpp
+++
@@ -1,7 +0,0 @@
-#include<windows.h>
-void main()
-{
-	//MessageBoxA(0, "Nuclear Launch Detected!", "Warning", 1);
-	//ShellExecuteA(0, "open", "notepad", 0, 0, 1);
-	ShellExecuteA(0, "open", "http://frozensky.sinaapp.com", 0, 0, 1);
-}
