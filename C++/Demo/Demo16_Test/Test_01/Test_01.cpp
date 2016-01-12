1fe244c12f6c0d6cbdf090fcee376e97a8d64332
Bowen Li <517066422@qq.com>
2016-01-12 09:11:12 +0800

    Demo_Demo16 2016-01-12
    Test_01

---
+++ C++/Demo/Demo16_Test/Test_01/Test_01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main01()
+{
+	enum myEnum {
+		lixiaoyao = 1,
+		zhaolinger = 2,
+		linyueru,
+
+		yuntianhe = 10,
+		hanlingsha = 11,
+		liumengli,
+		murongziying,
+	};
+	cout << murongziying << endl;
+	system("pause");
+	return 0;
+}
