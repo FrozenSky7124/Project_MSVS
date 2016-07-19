057a9722853eac6a48c02b36b500cfec132d8835
Bowen Li <517066422@qq.com>
2016-07-20 06:59:44 +0800

    PostColorfull v1.0

--- C++/FrozenSky/PostColorfull/mainwindow.cpp
+++ C++/FrozenSky/PostColorfull/mainwindow.cpp
@@ -1,26 +1,31 @@
 #include "mainwindow.h"
 #include <QMessageBox>
+#include <QFont>
 
 #define Author "FrozenSky"
-#define StartColor #66CCFF
-#define EndColor #FFFFFF
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
 {
     //MainWindow UI
     this->setWindowTitle("PostColorfull");
+    this->setWindowIcon(QIcon(":/icon.ico"));
 
     layout = new QGridLayout(this);
     OldTextEdit = new QTextEdit(this);
     NewTextEdit = new QTextEdit(this);
     ConvButton = new QPushButton(this);
 
-    ConvButton->setText("Convert");
+    ConvButton->setText("Convert && Copy");
+    OldTextEdit->setFont(QFont("微软雅黑", 12));
 
-    layout->addWidget(OldTextEdit, 0, 0, 1, 6);
-    layout->addWidget(ConvButton, 0, 6, 1, 1);
-    layout->addWidget(NewTextEdit, 0, 7, 1, 6);
+//    layout->addWidget(OldTextEdit, 0, 0, 1, 6);
+//    layout->addWidget(ConvButton, 0, 6, 1, 1);
+//    layout->addWidget(NewTextEdit, 0, 7, 1, 6);
+
+    layout->addWidget(OldTextEdit, 0, 0, 7, 10);
+    layout->addWidget(ConvButton, 8, 4, 1, 2);
+    layout->addWidget(NewTextEdit, 9, 0, 4, 10);
 
     //Slots Function
     connect(ConvButton, SIGNAL(clicked(bool)), this, SLOT(onConvertBtn()));
@@ -33,16 +38,51 @@ Widget::~Widget()
 
 void Widget::onConvertBtn()
 {
-    //
+    //染色函数
+    NewTextEdit->clear();
+
+    int arrayColor[18] =
+    {
+        0x000000, 0x330000, 0x660000, 0x990000, 0xCC0000, 0xFF0000,
+        0xFF3300, 0xCC3300, 0xFF6600, 0xFF9900, 0x009900, 0x339900,
+        0x00CC00, 0x33CC00, 0x66CC00, 0x00FF00, 0x66FF00, 0xCCFF00,
+    };   
+    int arrayColor_Backup[18] =
+    {
+        0x000000, 0x330000, 0x660000, 0x990000, 0xCC0000, 0xFF0000,
+        0xFF3300, 0xCC3300, 0xFF6600, 0xFF9900, 0x009900, 0x339900,
+        0x00CC00, 0x33CC00, 0x66CC00, 0x00FF00, 0x66FF00, 0xCCFF00,
+    };
     QString oldStr;
     QString newStr;
-
+    int n = 0;
     oldStr = OldTextEdit->toPlainText();
 
-    QChar cChar;
-    cChar = oldStr.at(3);
-    newStr = QString(cChar);
+    int oldStrLen = oldStr.length();
+    for (int i = 0; i < oldStrLen; i++)
+    {
+        QChar tmpChar;
+        QString tmpStr;
+        tmpChar = oldStr.at(i); //读取待转换字符串第i个字符
+        if (QString(tmpChar) == "\n") //换行时颜色标识n+1
+        {
+            n++;
+            if(n >= 17) //超出色彩循环范围则重置颜色标识n和基础色数组
+            {
+                n = 0;
+                memcpy_s(&arrayColor, 18 * sizeof(int), &arrayColor_Backup, 18 * sizeof(int));
+            }
+            tmpStr = QString(tmpChar);
+        }
+        else
+        {
+            tmpStr.sprintf("[color=%x]%s[/color]", arrayColor[n] += 0x000007, QString(tmpChar).toStdString().data());
+        }
+        newStr += tmpStr;
+    }
 
     NewTextEdit->append(newStr);
+    NewTextEdit->selectAll();
+    NewTextEdit->copy();
     //QMessageBox::information(this, "Main", oldStr);
 }
