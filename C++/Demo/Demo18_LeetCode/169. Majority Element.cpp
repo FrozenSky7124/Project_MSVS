f1f8e55c08f2d03b41bc0e71bd66b432dfd436d8
Bowen Li <517066422@qq.com>
2016-08-24 11:41:19 +0800

    Demo_LeetCode_169

---
+++ C++/Demo/Demo18_LeetCode/169. Majority Element.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int majorityElement(vector<int>& nums) {
+		int major = nums[0];
+		int count = 1;
+		for (int i = 1; i < nums.size(); i++)
+		{
+			if (count == 0)
+			{
+				major = nums[i];
+				count++;
+			}
+			else if (nums[i] == major)
+			{
+				count++;
+			}
+			else
+			{
+				count--;
+			}
+		}
+		return major;
+	}
+};
