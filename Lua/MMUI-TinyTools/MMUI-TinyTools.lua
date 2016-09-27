518c2da3f68da47ea4ef22c72807b3fb7e325be5
Bowen Li <517066422@qq.com>
2016-09-27 08:23:55 +0800

    MMUI-TinyTools Update

--- Lua/MMUI-TinyTools/MMUI-TinyTools.lua
+++ Lua/MMUI-TinyTools/MMUI-TinyTools.lua
@@ -1,38 +1,86 @@
 ﻿--[网格界面校正] 
-SLASH_EA1 = "/align" 
+local grid
+local boxSize = 32
 
-local f 
+function Grid_Show()
+	if not grid then
+        Grid_Create()
+	elseif grid.boxSize ~= boxSize then
+        grid:Hide()
+        Grid_Create()
+    else
+		grid:Show()
+	end
+end
 
-SlashCmdList["EA"] = function() 
-if f then 
-f:Hide() 
-f = nil 
-else 
-f = CreateFrame('Frame', nil, UIParent) 
-f:SetAllPoints(UIParent) 
-local w = GetScreenWidth() / 64 
-local h = GetScreenHeight() / 36 
-for i = 0, 64 do 
-local t = f:CreateTexture(nil, 'BACKGROUND') 
-if i == 32 then 
-t:SetTexture(1, 0, 0, 0.5) 
-else 
-t:SetTexture(0, 0, 0, 0.5) 
-end 
-t:SetPoint('TOPLEFT', f, 'TOPLEFT', i * w - 1, 0) 
-t:SetPoint('BOTTOMRIGHT', f, 'BOTTOMLEFT', i * w + 1, 0) 
-end 
-for i = 0, 36 do 
-local t = f:CreateTexture(nil, 'BACKGROUND') 
-if i == 18 then 
-t:SetTexture(1, 0, 0, 0.5) 
-else 
-t:SetTexture(0, 0, 0, 0.5) 
-end 
-t:SetPoint('TOPLEFT', f, 'TOPLEFT', 0, -i * h + 1) 
-t:SetPoint('BOTTOMRIGHT', f, 'TOPRIGHT', 0, -i * h - 1) 
-end 
-end 
+function Grid_Hide()
+	if grid then
+		grid:Hide()
+	end
+end
+
+local isAligning = false
+
+SLASH_TOGGLEGRID1 = "/align"
+SlashCmdList["TOGGLEGRID"] = function(arg)
+    if isAligning then
+        Grid_Hide()
+        isAligning = false
+    else
+        boxSize = (math.ceil((tonumber(arg) or boxSize) / 32) * 32)
+    if boxSize > 256 then boxSize = 256 end    
+        Grid_Show()
+        isAligning = true
+    end
+end
+
+function Grid_Create() 
+	grid = CreateFrame('Frame', nil, UIParent) 
+	grid.boxSize = boxSize 
+	grid:SetAllPoints(UIParent) 
+
+	local size = 2 
+	local width = GetScreenWidth()
+	local ratio = width / GetScreenHeight()
+	local height = GetScreenHeight() * ratio
+
+	local wStep = width / boxSize
+	local hStep = height / boxSize
+
+	for i = 0, boxSize do 
+		local tx = grid:CreateTexture(nil, 'BACKGROUND') 
+		if i == boxSize / 2 then 
+			tx:SetColorTexture(1, 0, 0, 0.5) 
+		else 
+			tx:SetColorTexture(0, 0, 0, 0.5) 
+		end 
+		tx:SetPoint("TOPLEFT", grid, "TOPLEFT", i*wStep - (size/2), 0) 
+		tx:SetPoint('BOTTOMRIGHT', grid, 'BOTTOMLEFT', i*wStep + (size/2), 0) 
+	end 
+	height = GetScreenHeight()
+	
+	do
+		local tx = grid:CreateTexture(nil, 'BACKGROUND') 
+		tx:SetColorTexture(1, 0, 0, 0.5)
+		tx:SetPoint("TOPLEFT", grid, "TOPLEFT", 0, -(height/2) + (size/2))
+		tx:SetPoint('BOTTOMRIGHT', grid, 'TOPRIGHT', 0, -(height/2 + size/2))
+	end
+	
+	for i = 1, math.floor((height/2)/hStep) do
+		local tx = grid:CreateTexture(nil, 'BACKGROUND') 
+		tx:SetColorTexture(0, 0, 0, 0.5)
+		
+		tx:SetPoint("TOPLEFT", grid, "TOPLEFT", 0, -(height/2+i*hStep) + (size/2))
+		tx:SetPoint('BOTTOMRIGHT', grid, 'TOPRIGHT', 0, -(height/2+i*hStep + size/2))
+		
+		tx = grid:CreateTexture(nil, 'BACKGROUND') 
+		tx:SetColorTexture(0, 0, 0, 0.5)
+		
+		tx:SetPoint("TOPLEFT", grid, "TOPLEFT", 0, -(height/2-i*hStep) + (size/2))
+		tx:SetPoint('BOTTOMRIGHT', grid, 'TOPRIGHT', 0, -(height/2-i*hStep + size/2))
+		
+	end
+	
 end
 
 
--- Lua/MMUI-TinyTools/PriestBar.lua
+++ Lua/MMUI-TinyTools/PriestBar.lua
@@ -5,3 +5,6 @@ PriestBarFrame:ClearAllPoints()
 PriestBarFrame:SetPoint("CENTER", UIParent, "CENTER", 0, -352)
 PriestBarFrame:SetScale(1.2)
 
+--[强制关闭WQT插件世界任务追踪]
+--WorldQuestTrackerScreenPanel:ClearAllPoints()
+--WorldQuestTrackerScreenPanel:Hide()
