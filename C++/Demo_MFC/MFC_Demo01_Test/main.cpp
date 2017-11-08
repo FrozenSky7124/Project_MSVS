8813f84d507c56edee9545ce7a9e58cbab0b2e27
Bowen Li <517066422@qq.com>
2017-11-09 00:47:40 +0800

    Project : MFC_Demo01 Update

--- C++/Demo_MFC/MFC_Demo01_Test/main.cpp
+++ C++/Demo_MFC/MFC_Demo01_Test/main.cpp
@@ -2,5 +2,24 @@
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
-	return MessageBox(NULL, "This is a Demo : MFC_Demo01.", "Demo01", 0);
+	//使用SendMessage()函数来实现鼠标点击的坐标换算方法
+	//LPARAM lparam = MAKELPARAM(50, 90);
+	 
+	HWND hWnd = FindWindow(NULL, "Calculator");
+	int ret = MessageBox(hWnd, "This is a Demo : MFC_Demo01", "Demo01", MB_YESNO | MB_ICONINFORMATION);
+	switch (ret)
+	{
+	case 0:
+		//RunError
+		break;
+	case IDNO:
+		MessageBox(hWnd, "You Press NO.", "Demo01", 0);
+		break;
+	case IDYES:
+		MessageBox(hWnd, "You Press YES.", "Demo01", 0);
+		break;
+	default:
+		break;
+	}
+	return 0;
 }
