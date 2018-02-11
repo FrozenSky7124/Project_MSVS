6738344d0132b48cc1d24a5426d20d6fb57cc5a3
Bowen Li <517066422@qq.com>
2018-02-11 21:09:50 +0800

    MFC_Demo12 CDib
    Class CDib Testing Project

---
+++ C++/Demo_MFC/Demo12_CDib/CDibDlg.h
@@ -0,0 +1,36 @@
+
+// CDibDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CDibDlg 对话框
+class CDibDlg : public CDialogEx
+{
+// 构造
+public:
+	CDibDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DEMO12_CDIB_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+	CDC*  m_pCDC;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButtonShow();
+};
