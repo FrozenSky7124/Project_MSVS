8a3f8603fef15de31ddb421b1532a81ccd1199eb
FrozenSky <517066422@qq.com>
2018-06-08 00:42:17 +0800

    [^] Fix bug: Memory exception
    Fix bug: release buffer exception

--- C++/FrozenSky/DLL_CDib/Dib_Ex.cpp
+++ C++/FrozenSky/DLL_CDib/Dib_Ex.cpp
@@ -792,7 +792,11 @@ void CDib::Empty(BOOL bFlag)
 	// 释放预构建颜色表内存
 	if (m_lpRgbQuad_Mem != NULL)
 	{
-		delete [] m_lpRgbQuad_Mem;
+		//for (UINT i = 0; i < m_iClrUse; i++)
+		//{
+		//	delete [] (m_lpRgbQuad_Mem + i);
+		//}
+		delete[] m_lpRgbQuad_Mem;
 		m_lpRgbQuad_Mem = NULL;
 	}
 
